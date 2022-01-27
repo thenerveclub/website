ab722d160b7cfe41d8cd59e1e737f8212e63cf1c
christophkoenekamp <christoph.koenekamp@icloud.com>
2022-01-27 18:44:26 +0100

    update

--- src/ZTopspent1.js
+++ src/ZTopspent1.js
@@ -10,14 +10,23 @@ const Top = `
 }
 `;
 
+const Change = `
+{
+  matic-network {
+    usd
+  }
+}
+`;
+
 export default function ZEarnt() {
   const top = useTop();
+  const change = useChange();
 
   return (
     <div>
       <ul style={{ listStyle: "none" }}>
         {top.map((top) => (
-          <li key={top.id}>{top.spent/1.e+18}</li>
+          <li key={top.id}>USD {top.spent/1.e+18}{change.usd}</li>
         ))}
       </ul>
     </div>
@@ -38,4 +47,25 @@ function useTop() {
   }, []);
 
   return top;
+}
+
+
+function useChange() {
+  const [change, setChange] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://api.coingecko.com/api/v3/simple/price?ids=matic-network&vs_currencies=usd", {
+      method: "POST",
+      headers: { 
+        "Accept": "application/json",
+        "Content-Type": "application/json; charset=utf-8", 
+        "cache-control": "max-age=30,public,must-revalidate,s-maxage=60"
+      },
+      body: JSON.stringify({ query: Change })
+    })
+      .then((response) => response.json())
+      .then((data) => setChange(data.data["matic-network"]));
+  }, []);
+
+  return change;
 }
