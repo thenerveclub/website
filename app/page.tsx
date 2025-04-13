e17ad1a0b37f537c0f96399ddf540c0a9cfc5beb
Christoph Könekamp <55452146+thekoenekamp@users.noreply.github.com>
2025-04-13 21:24:34 +0200

    add shimmer

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,11 @@ export default function Landing() {
 		<>
 			<div className="font-baskervville min-h-screen flex justify-center items-center p-0">
 				<div className="flex flex-col items-center justify-center w-[80%] cursor-default text-center sm:w-[95%]">
-					<h1 className="text-4xl xl:text-7xl xxl:text-8xl font-light uppercase text-transparent bg-[linear-gradient(350.21deg,_white_-13.99%,_rgba(255,255,255,0)_136.74%)] bg-clip-text [-webkit-background-clip:text] mb-10">
-						Nerve Global
-					</h1>
+					<div className="shine-text-container text-4xl xl:text-7xl xxl:text-8xl font-light uppercase mb-10">
+						<span className="shine-text-base">Nerve Global</span>
+						<span className="shine-text-overlay">Nerve Global</span>
+					</div>
+
 					<p className="text-xl xl:text-4xl xxl:text-6xl font-light uppercase text-transparent bg-[linear-gradient(350.21deg,_white_-13.99%,_rgba(255,255,255,0)_136.74%)] bg-clip-text [-webkit-background-clip:text] no-underline mt-4 sm:text-[1.5rem]">
 						We exclusively work on unique ideas in the emerging
 						<br />
